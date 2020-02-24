f45f11a857bc19cdde940c87e6e3b3449d2c5b5a
zhangcn <15236674712@163.com>
2020-02-24 22:52:46 +0800

    第idea 1次提交

--- src/test/java/com/baizhi/testdemo/TestDemoApplicationTests.java
+++ src/test/java/com/baizhi/testdemo/TestDemoApplicationTests.java
@@ -17,6 +17,7 @@ public class TestDemoApplicationTests {
         System.out.println("44444");
         System.out.println("dev add ");
         System.out.println("Github add ");
+        System.out.println("IDEA add ");
     }
 
 }
