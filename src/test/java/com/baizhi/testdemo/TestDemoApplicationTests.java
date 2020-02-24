eebfcb1e321d5bc6931c8554f6d997977cb66920
15236674712 <49543534+15236674712@users.noreply.github.com>
2020-02-24 22:42:21 +0800

    Github添加

--- src/test/java/com/baizhi/testdemo/TestDemoApplicationTests.java
+++ src/test/java/com/baizhi/testdemo/TestDemoApplicationTests.java
@@ -16,6 +16,7 @@ public class TestDemoApplicationTests {
         System.out.println("111");
         System.out.println("44444");
         System.out.println("dev add ");
+        System.out.println("Github add ");
     }
 
 }
